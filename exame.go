6b1f197dbf20365d8e91574b7962978746d26070
drjoaobatista <d@joaobatista.eng.br>
2020-05-06 00:42:19 -0300

    adicionando o exame no modelo

---
+++ exame.go
@@ -0,0 +1,10 @@
+package epidemiologicalsimulation
+
+//cidade rede mundo pequeno
+type exame struct {
+	nome                  string
+	especificidade        float32
+	sensibilidadeRelativa float32
+	precisão              float32
+	concordância          float32
+}
