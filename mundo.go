7cf3c4ada267edae113717d215e1526e5f2aa87b
drjoaobatista <dr@joabatista.eng.br>
2020-06-10 01:25:30 -0300

    coreeções e testes

--- mundo.go
+++ mundo.go
@@ -4,17 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"math/rand"
 	"os"
 	"runtime"
 	"strconv"
 )
 
-//#TODO: fundir mundo e Mundo
 //Mundo estura para armazenar rede longa distância
 type Mundo struct {
 	nomesCidades             []string
-	populaçãoCidades         []string
+	populaçãoCidades         []int
 	numeroCidades            int
 	tamanhoPopulaçãoQuadrada int
 	tamanhoPopulação         int
@@ -29,88 +29,166 @@ type Mundo struct {
 	arquivoPopulaçãoCidades  string
 	arquivoDistanciasCidades string
 	TempoSimulação           int
+	numeroVizinhos           int
 	// funcao de probabilidade da contaminaçao
 	f func(int) float32
 }
 
-func (m Mundo) carregaArquivos() bool {
+func (m *Mundo) carregaNomeCidades() bool {
 	if m.arquivoNomesCidades == "" {
 		fmt.Println("nome do arquivoNome")
 		return false
 	} else {
-		nomes, err := lerTexto(m.arquivoNomesCidades)
+		arquivo, err := os.Open(m.arquivoNomesCidades)
+		// Caso tenha encontrado algum erro ao tentar abrir o arquivo retorne o erro encontrado
 		if err != nil {
-			log.Fatalf("Erro: %v", err)
+			log.Fatalf("Erro ler nomes: %v", err)
+			return false
 		}
-		m.nomesCidades = nomes
+		// Garante que o arquivo sera fechado apos o uso
+		defer arquivo.Close()
+		// Cria um scanner que lê cada linha do arquivo
+		scanner := bufio.NewScanner(arquivo)
+		for scanner.Scan() {
+			m.nomesCidades = append(m.nomesCidades, scanner.Text())
+		}
 	}
+	if m.cidades == nil {
+		m.numeroCidades = len(m.nomesCidades)
+		m.cidades = make([]Cidade, m.numeroCidades)
+	}
+	for i, nome := range m.nomesCidades {
+		m.cidades[i] = Cidade{
+			codCidade: uint8(i),
+			nome:      nome,
+		}
+	}
+	return true
+}
+
+//-----------------------------------
+func (m *Mundo) carregaPopulaçãoCidades() bool {
 	if m.arquivoPopulaçãoCidades == "" {
+		fmt.Println("nome do arquivoPopulaçãoCidades")
+		return false
+	} else {
+		arquivo, err := os.Open(m.arquivoPopulaçãoCidades)
+		// Caso tenha encontrado algum erro ao tentar abrir o arquivo retorne o erro encontrado
+		if err != nil {
+			log.Fatalf("Erro ler nomes: %v", err)
+			return false
+		}
+		// Garante que o arquivo sera fechado apos o uso
+		defer arquivo.Close()
+		// Cria um scanner que lê cada linha do arquivo
+		var linhas []string
+		scanner := bufio.NewScanner(arquivo)
+		for scanner.Scan() {
+			linhas = append(linhas, scanner.Text())
+		}
+		m.populaçãoCidades = make([]int, len(linhas))
+		if m.cidades == nil {
+			m.numeroCidades = len(linhas)
+			m.cidades = make([]Cidade, len(linhas))
+		}
+		for i := range linhas {
+			pop, err := strconv.ParseInt(linhas[i], 10, 64)
+			if err != nil {
+				log.Fatalf("Erro: %v", err)
+				return false
+			}
+			m.populaçãoCidades[i] = int(pop)
+			m.tamanhoPopulação += int(pop)
+			m.cidades[i] = Cidade{
+				tamanhoPopulação: int(pop),
+			}
+		}
+	}
+	return true
+}
+
+//-----------------------------------
+func (m *Mundo) carregaDistânciasCidades() bool {
+	if m.arquivoDistanciasCidades == "" {
 		fmt.Println("nome do arquivoNome")
 		return false
 	} else {
-		populaçãoCidade, err := lerTexto(m.arquivoPopulaçãoCidades)
+		arquivo, err := os.Open(m.arquivoDistanciasCidades)
+		// Caso tenha encontrado algum erro ao tentar abrir o arquivo retorne o erro encontrado
 		if err != nil {
-			log.Fatalf("Erro: %v", err)
+			log.Fatalf("Erro ler nomes: %v", err)
+			return false
 		}
-		m.populaçãoCidades = populaçãoCidade
-	}
-	if m.arquivoDistanciasCidades == "" {
-		fmt.Println("nome do arquivoDistanciasCidades ausente")
-		return false
-	} else {
-		_, err := lerTexto(m.arquivoDistanciasCidades)
-		if err != nil {
-			log.Fatalf("Erro: %v", err)
+		// Garante que o arquivo sera fechado apos o uso
+		defer arquivo.Close()
+		// Cria um scanner que lê cada linha do arquivo
+		var linhas []string
+		scanner := bufio.NewScanner(arquivo)
+		for scanner.Scan() {
+			linhas = append(linhas, scanner.Text())
 		}
-
+		m.populaçãoCidades = make([]int, len(linhas))
+		if m.cidades == nil {
+			m.numeroCidades = int(math.Sqrt(float64(len(linhas))))
+			m.cidades = make([]Cidade, m.numeroCidades)
+		}
+		distância := make([]float32, len(linhas))
+		for i := range linhas {
+			dist, err := strconv.ParseFloat(linhas[i], 32)
+			if err != nil {
+				log.Fatalf("Erro: %v", err)
+				return false
+			}
+			distância[i] = float32(dist)
+		}
+		m.distâncias = make([][]float32, m.numeroCidades)
+		for i := range m.distâncias {
+			m.distâncias[i] = make([]float32, m.numeroCidades)
+		}
+		k := 0
+		for i := 0; i < m.numeroCidades; i++ {
+			for j := 0; j < m.numeroCidades; j++ {
+				m.distâncias[i][j] = distância[k]
+				k++
+			}
+		}
 	}
-
 	return true
 }
 
 //init carrega do disco três arquivos com o nome da Cidade e a população e as distâncias
 func (m *Mundo) init() bool {
-	if m.carregaArquivos() {
-		var populacaoQuadrada int
-		var populaçãoTotal int
-		m.cidades = make([]Cidade, m.numeroCidades)
-		for i, nome := range m.nomesCidades {
-			pop, err := strconv.ParseInt(m.populaçãoCidades[i], 10, 64)
-			if err != nil {
-				log.Fatalf("Erro: %v", err)
-			}
-			populaçãoTotal += int(pop)
-			m.cidades[i] = Cidade{
-				tamanhoPopulação: int(pop),
-				codCidade:        uint8(i),
-				nome:             nome,
-			}
-			populacaoQuadrada += m.cidades[i].init()
-		}
-		m.tamanhoPopulaçãoQuadrada = populacaoQuadrada
-		m.tamanhoPopulação = populaçãoTotal
-		//criando a populacao mundial
-		m.população = make([]Pessoa, m.tamanhoPopulaçãoQuadrada)
-		inicio := 0
-		//distribuindo a populacao mundial
-		for i := 0; i < m.numeroCidades; i++ {
-			fim := int(m.cidades[i].tamanhoPopulaçãoQuadrada) + inicio
-			m.cidades[i].população = m.população[inicio:fim]
-			m.cidades[i].vizinhos()
-			m.cidades[i].setPessoa()
-			inicio = int(fim)
-		}
-		m.initProbabilidadeContagio(m.f)
+
+	m.carregaNomeCidades()
+	m.carregaPopulaçãoCidades()
+	m.initProbabilidadeContagio()
+	for i := 0; i < m.numeroCidades; i++ {
+		m.tamanhoPopulaçãoQuadrada += m.cidades[i].init()
+	}
+	//criando as pessoas do mundo
+	m.população = make([]Pessoa, m.tamanhoPopulaçãoQuadrada)
+
+	//distribuindo a populacao mundial nas cidades
+	inicio := 0
+	for i := 0; i < m.numeroCidades; i++ {
+		fim := int(m.cidades[i].tamanhoPopulaçãoQuadrada) + inicio
+		m.cidades[i].população = m.população[inicio:fim]
+		m.cidades[i].vizinhos()
+		m.cidades[i].setPessoa()
+		inicio = int(fim)
 	}
 	return true
+	// m.initProbabilidadeContagio(m.f)
+
 }
 
 //initProbabilidadeContagio inicaça a função de porbabilidade de contagio
-func (m *Mundo) initProbabilidadeContagio(f func(int) float32) {
-	p := make([]float32, 5)
+func (m *Mundo) initProbabilidadeContagio() {
+	p := make([]float32, m.numeroVizinhos)
 	for i := range p {
-		p[i] = f(i)
+		p[i] = m.f(i)
 	}
+	m.probabilidadeContagio = make([]float32, m.numeroVizinhos)
 	copy(m.probabilidadeContagio, p)
 }
 
--- mundo_test.go
+++ mundo_test.go
@@ -1,16 +1,63 @@
 package epidemiologicalsimulation
 
-import "testing"
+import (
+	"log"
+	"math"
+	"testing"
+)
 
 var sergipe = Mundo{
 	arquivoNomesCidades:      "testeNomes.dat",
 	arquivoPopulaçãoCidades:  "testepopulacao.dat",
 	arquivoDistanciasCidades: "testedistancias.dat",
 	TempoSimulação:           10,
+	numeroVizinhos:           5,
+	f: func(n int) float32 {
+		return float32(1 - math.Pow((1-0.1), float64(n)))
+	},
 }
 
-func TestCarregaArquivos(t *testing.T) {
-	obtido := sergipe.carregaArquivos()
+func TestLerTexto(t *testing.T) {
+	obtido, err := lerTexto("testeNomes.dat")
+	if err != nil {
+		log.Fatalf("Erro ler nomes: %v", err)
+	}
+	//desejado := []string{"Aracaju", "Lagarto"}
+	if obtido == nil {
+		t.Errorf("o valor obtido foi %v ", obtido)
+	}
+
+	obtido2 := len(obtido)
+	desejado := 2
+	if obtido2 != desejado {
+		t.Errorf("o valor obtido foi %v e o desejado foi %v", obtido2, desejado)
+	}
+
+}
+
+func TestCarregaNomesCidades(t *testing.T) {
+	obtido := sergipe.carregaNomeCidades()
+	desejado := true
+	if obtido != desejado {
+		t.Errorf("o valor obtido foi %v e o desejado foi %v", obtido, desejado)
+	}
+	if sergipe.nomesCidades == nil {
+		t.Errorf("o valor obtido foi %v ", sergipe.nomesCidades)
+	}
+
+}
+
+func TestCarregapopulaçãoCidades(t *testing.T) {
+	obtido := sergipe.carregaPopulaçãoCidades()
+	desejado := true
+	if obtido != desejado {
+		t.Errorf("o valor obtido foi %v e o desejado foi %v", obtido, desejado)
+	}
+
+}
+
+func TestCarregaDistânciasCidades(t *testing.T) {
+	obtido := sergipe.carregaDistânciasCidades()
 	desejado := true
 	if obtido != desejado {
 		t.Errorf("o valor obtido foi %v e o desejado foi %v", obtido, desejado)
@@ -20,6 +67,33 @@ func TestCarregaArquivos(t *testing.T) {
 func TestInitMundo(t *testing.T) {
 	obtido := sergipe.init()
 	desejado := true
+	if obtido != desejado {
+		t.Errorf("o valor obtido foi %v e o desejado foi %v", obtido, desejado)
+	}
+
+	obtido2 := sergipe.numeroCidades
+	desejado2 := 2
+	if obtido2 != desejado2 {
+		t.Errorf("o valor obtido foi %v e o desejado foi %v", obtido2, desejado2)
+	}
+
+	obtido1 := sergipe.cidades[0].tamanhoPopulação
+	desejado1 := 10000
+	if obtido1 != desejado1 {
+		t.Errorf("o valor obtido foi %v e o desejado foi %v", obtido1, desejado1)
+	}
+
+	obtido3 := sergipe.cidades[1].tamanhoPopulação
+	desejado3 := 1000
+	if obtido1 != desejado1 {
+		t.Errorf("o valor obtido foi %v e o desejado foi %v", obtido3, desejado3)
+	}
+}
+
+func TestInitProbabilidadeContagio(t *testing.T) {
+	sergipe.initProbabilidadeContagio()
+	obtido := sergipe.probabilidadeContagio[0]
+	desejado := float32(0.0)
 	if obtido != desejado {
 		t.Errorf("o valor obtido foi %v e o desejado foi %v", obtido, desejado)
 	}
